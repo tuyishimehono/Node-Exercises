2d5c613c6e6cf2bee3ad07b7b61ce149684bc98f
tuyishimehono <tuhonori1@gmail.com>
2024-09-13 09:28:26 +0200

    change port

--- Mental-Model-CRUD/app.js
+++ Mental-Model-CRUD/app.js
@@ -49,7 +49,7 @@ app.delete("/output", (req, res) => {
 });
 
 const server = http.createServer(app);
-const PORT = 8000;
+const PORT = 7000;
 
 server.listen(PORT, () => {
   console.log("Server is listening...");
